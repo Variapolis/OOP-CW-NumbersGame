2abf36a5c1318738d72417f5328736a318095df3
Vari <tmarksinbox@gmail.com>
2020-12-08 03:41:01 +0000

    Update CompositeNumber.cpp

--- src/CompositeNumber.cpp
+++ src/CompositeNumber.cpp
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-
-=======
->>>>>>> parent of 4bfa74a... Restarts after winning
-=======
->>>>>>> parent of 4bfa74a... Restarts after winning
 #include "CompositeNumber.h"
 
 
